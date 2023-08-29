57f37a27b7aa246efddb1db11c791837c84e0639
gauravwtf <raogaurav7007@gmail.com>
2023-08-29 17:03:34 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -11,18 +11,20 @@ app = Flask(__name__)
 @verify_key_decorator(CLIENT_PUBLIC_KEY)
 def interactions():
     if request.json['type'] == InteractionType.APPLICATION_COMMAND:
+        print(request.json)
+        # just a test, i dont know what the response looks like
+        if request.json['data']['name'] == "hi":
+            return jsonify({
+                'type': InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                'data': {
+                    'content': f"Hello {request.json['data']['options'][0]['value']}",
+                "flags": InteractionResponseFlags.EPHEMERAL
+                }
+            })
         return jsonify({
             'type': InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
             'data': {
                 'content': 'Hello world'
-            }
-        })
-    elif request.json['type'] == InteractionType.MESSAGE_COMPONENT:
-        return jsonify({
-            'type': InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-            'data': {
-                'content': 'Hello, you interacted with a component.',
-                'flags': InteractionResponseFlags.EPHEMERAL
             }
         })
 
