dea23b43cbc3bc83dffc634c460ebe442afeebee
gauravwtf <raogaurav7007@gmail.com>
2023-08-29 17:03:40 +0530

    Create commands.py

---
+++ src/commands/commands.py
@@ -0,0 +1,26 @@
+commands = [
+    {
+        "type": 1,
+        "name": "hi",
+        "options": [
+            {
+                "name": "user",
+                "description": "The user to say hi",
+                "type": 6,
+                "required": True,
+            }
+        ],
+    },
+    {
+        "type": 1,
+        "name": "bye",
+        "options": [
+            {
+                "name": "user",
+                "description": "The user to say bye",
+                "type": 6,
+                "required": True,
+            }
+        ],
+    },
+]
