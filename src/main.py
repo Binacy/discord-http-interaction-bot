92208b9950a513e2b4bf97a0e10281a91282fd59
gauravwtf <raogaurav7007@gmail.com>
2023-08-29 16:13:06 +0530

    fuck quart, use flask guys

--- src/main.py
+++ src/main.py
@@ -1,29 +1,30 @@
 import config
-from quart import Quart, request
+from flask import Flask, jsonify, request
 from discord_interactions import verify_key_decorator, InteractionType, InteractionResponseType, InteractionResponseFlags
+
 
 CLIENT_PUBLIC_KEY = config.CLIENT_PUBLIC_KEY
 
-app = Quart(__name__)
+app = Flask(__name__)
 
 @app.route('/interactions', methods=['POST'])
 @verify_key_decorator(CLIENT_PUBLIC_KEY)
-async def interactions():
+def interactions():
     if request.json['type'] == InteractionType.APPLICATION_COMMAND:
-        return {
+        return jsonify({
             'type': InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
             'data': {
                 'content': 'Hello world'
             }
-        }
+        })
     elif request.json['type'] == InteractionType.MESSAGE_COMPONENT:
-        return {
+        return jsonify({
             'type': InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
             'data': {
                 'content': 'Hello, you interacted with a component.',
                 'flags': InteractionResponseFlags.EPHEMERAL
             }
-        }
+        })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
