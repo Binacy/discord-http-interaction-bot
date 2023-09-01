73925fc366cb832571336a142e0684e20fdee898
NotAditya69 <ytzlegend46@gmail.com>
2023-09-01 20:06:22 +0530

    fix: Add validation for signature (bugged)

--- src/main.py
+++ src/main.py
@@ -1,32 +1,65 @@
 import config
-from flask import Flask, jsonify, request
-from discord_interactions import verify_key_decorator, InteractionType, InteractionResponseType, InteractionResponseFlags
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from discord_interactions import InteractionType, InteractionResponseType, InteractionResponseFlags
+from constants import InteractionType, InteractionResponseType, InteractionResponseFlags, verify_key_decorator
 
+app = FastAPI()
 
 CLIENT_PUBLIC_KEY = config.CLIENT_PUBLIC_KEY
+CLIENT_SECRET_KEY = config.CLIENT_SECRET_KEY
 
-app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
-@verify_key_decorator(CLIENT_PUBLIC_KEY)
-def interactions():
-    if request.json['type'] == InteractionType.APPLICATION_COMMAND:
-        print(request.json)
-        # just a test, i dont know what the response looks like
-        if request.json['data']['name'] == "hi":
-            return jsonify({
-                'type': InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-                'data': {
-                    'content': f"Hello {request.json['data']['options'][0]['value']}",
-                "flags": InteractionResponseFlags.EPHEMERAL
-                }
-            })
-        return jsonify({
-            'type': InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-            'data': {
-                'content': 'Hello world'
+class InteractionData(BaseModel):
+    name: str
+    options: list
+
+
+@app.post("/")
+async def interactions(request: Request, interaction: InteractionData):
+    json_data = await request.json()
+
+    signature = request.headers.get("X-Signature-Ed25519")
+    timestamp = request.headers.get("X-Signature-Timestamp")
+
+    print("Yaha bhi sex waha vi sex har jagah sex")
+    print(signature)
+
+    if not signature or not timestamp:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
+    body = await request.body()
+    verify_key = hmac.new(
+        CLIENT_SECRET.encode(),
+        timestamp.encode() + body,
+        hashlib.sha256
+    ).hexdigest()
+
+    if not hmac.compare_digest(signature, verify_key):
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
+    if json_data["type"] == 1:  # Ping event
+        return {"type": 1}
+
+    if json_data["type"] == InteractionType.APPLICATION_COMMAND:
+        if json_data["data"]["name"] == "hi":
+            user_name = json_data["data"]["options"][0]["value"]
+            response_data = {
+                "type": InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                "data": {
+                    "content": f"Hello {user_name}",
+                    "flags": InteractionResponseFlags.EPHEMERAL,
+                },
             }
-        })
+        else:
+            response_data = {
+                "type": InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                "data": {
+                    "content": "Hello world",
+                },
+            }
+        return response_data
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=3000)
