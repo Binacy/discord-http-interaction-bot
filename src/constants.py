5d389ae379ef3e5cce348b02dfc8e15254c59c03
gauravwtf <raogaurav7007@gmail.com>
2023-09-09 19:45:43 +0530

    cleaner

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,3 @@
-from nacl.signing import VerifyKey
-
 class InteractionType:
     PING = 1
     APPLICATION_COMMAND = 2
@@ -18,13 +16,3 @@ class InteractionResponseType:
 
 class InteractionResponseFlags:
     EPHEMERAL = 1 << 6
-
-def verify_key(raw_body: bytes, signature: str, timestamp: str, client_public_key: str) -> bool:
-    message = timestamp.encode() + raw_body
-    try:
-        vk = VerifyKey(bytes.fromhex(client_public_key))
-        vk.verify(message, bytes.fromhex(signature))
-        return True
-    except Exception as ex:
-        print(ex)
-    return False
