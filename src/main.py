751e9dbc1d5c0ce632051c7202fe78c2dc3df21a
gauravwtf <raogaurav7007@gmail.com>
2023-09-09 19:46:11 +0530

    lemme add a command handler too

--- src/main.py
+++ src/main.py
@@ -1,47 +1,8 @@
-from dotenv import load_dotenv
 import uvicorn
-import os
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI, Request
 from starlette.middleware import Middleware
-from starlette.middleware.base import BaseHTTPMiddleware
-from constants import (
-    InteractionType,
-    InteractionResponseType,
-    InteractionResponseFlags,
-    verify_key,
-)
-
-load_dotenv()
-CLIENT_PUBLIC_KEY = os.environ.get("CLIENT_PUBLIC_KEY")
-
-
-async def set_body(request: Request, body: bytes):
-    async def receive():
-        return {"type": "http.request", "body": body}
-
-    request._receive = receive
-
-
-async def get_body(request: Request) -> bytes:
-    body = await request.body()
-    await set_body(request, body)
-    return body
-
-
-class CustomHeaderMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request, call_next):
-        signature = request.headers["X-Signature-Ed25519"]
-        timestamp = request.headers["X-Signature-Timestamp"]
-        request_body = await get_body(request)
-        if (
-            signature is None
-            or timestamp is None
-            or not verify_key(request_body, signature, timestamp, CLIENT_PUBLIC_KEY)
-        ):
-            return Response("Bad request signature", status_code=401)
-        response = await call_next(request)
-        return response
-
+from constants import InteractionType, InteractionResponseType, InteractionResponseFlags
+from verify import CustomHeaderMiddleware
 
 app = FastAPI(middleware=[Middleware(CustomHeaderMiddleware)])
 
